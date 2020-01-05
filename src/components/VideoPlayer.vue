68e76addbb9df3a03383277e65e8a7c3ef118126
zhaojun1998 <root@zhaojun.im>
2020-01-05 15:48:21 +0800

    优化 HLS 和 FLV 支持

--- src/components/VideoPlayer.vue
+++ src/components/VideoPlayer.vue
@@ -3,14 +3,17 @@
 </template>
 
 <script>
-    import Hls from 'hls.js'
-    window.Hls = Hls;
 
     import 'dplayer/dist/DPlayer.min.css';
     import axios from "axios";
 
     let DPlayer;
+    import flvjs from 'flv.js';
+    import Hls from 'hls.js';
     require.ensure([], function() { DPlayer = require('dplayer')}, 'dplayer');
+
+    window.flvjs = flvjs;
+    window.Hls = Hls;
 
     export default {
         name: "VideoPlayer",
@@ -21,13 +24,7 @@
                     video: {
                         url: ''
                     },
-                    subtitle: {
-                        url: axios.defaults.baseURL  + '/api/content/origin?url=http://c.jun6.net/testProcess.mp4.vtt',
-                        type: 'webvtt',
-                        fontSize: '25px',
-                        bottom: '10%',
-                        color: '#b7daff',
-                    },
+                    subtitle: null,
                     autoplay: false,
                 },
                 player: null,
@@ -36,12 +33,30 @@
         props: {
             url: String
         },
-        mounted() {
+        async mounted() {
             this.options.container = document.getElementById("dplayer");
+
+            let vttUrl = axios.defaults.baseURL  + '/api/content/origin?url=' + this.url + ".vtt";
+
+            await this.$http.get('api/content/exist', {params: {url: vttUrl}}).then((response) => {
+                if (response.data) {
+                    this.options.subtitle = {
+                        url: vttUrl,
+                        type: 'webvtt',
+                        fontSize: '25px',
+                        bottom: '10%',
+                        color: '#b7daff',
+                    };
+                }
+            });
+
             this.player = new DPlayer(this.options);
             this.player.switchVideo({
                 url: this.url
             })
+        },
+        destroyed() {
+            this.player.destroy();
         },
         watch: {
             url(val) {
