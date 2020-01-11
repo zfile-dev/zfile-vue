66235cda3c3c1c09282174f04622b93171ca4868
zhaojun1998 <root@zhaojun.im>
2020-01-12 01:01:38 +0800

    增强缓存管理模块

--- src/components/admin/CacheManager.vue
+++ src/components/admin/CacheManager.vue
@@ -1,74 +1,139 @@
 <template>
     <el-row>
-        <el-form :inline="true" :model="formData" class="demo-form-inline">
-            <el-form-item label="是否已开启缓存">
-                <el-switch v-model="formData.enableCache" disabled/>
-            </el-form-item>
-            <el-form-item label="是否已完成缓存">
-                <el-switch v-model="formData.cacheFinish" disabled/>
-            </el-form-item>
-            <el-form-item label="已缓存条目">
-                <span style="font-weight: bold; color: red" v-text="formData.cacheCount"/>
-            </el-form-item>
-            <el-form-item>
-                <el-button type="danger" size="small"
-                           :disabled="!formData.enableCache || !formData.cacheFinish" @click="clearAllCache" round>清理缓存</el-button>
-                <el-button type="primary" size="small"
-                           :disabled="formData.cacheFinish" @click="cacheAll" round>缓存所有</el-button>
-            </el-form-item>
-        </el-form>
-        <el-table
-                :data="tableData.filter(data => !formData.search || data.name.toLowerCase().includes(formData.search.toLowerCase()))"
-                style="width: 100%">
-            <el-table-column
-                    prop="name"
-                    label="缓存 Key (文件夹名称)"
-                    width="550">
-            </el-table-column>
-            <el-table-column>
-                <template slot="header" slot-scope="scope">
-                    <el-input
-                            v-model="formData.search"
-                            size="mini"
-                            placeholder="输入关键字搜索"/>
-                </template>
-                <template slot-scope="scope">
-                    <el-button
-                            size="mini"
-                            type="primary"
-                            round
-                            @click="refreshCache(scope.$index, scope.row)">刷新缓存</el-button>
-                </template>
-            </el-table-column>
-        </el-table>
+        <el-col>
+            <el-row :gutter="20">
+                <el-col :span="6">
+                    <el-card shadow="always">
+                        <div slot="header" class="clearfix">
+                            <span class="card-title">是否已开启缓存</span>
+                        </div>
+                        <el-switch v-model="enableCache" :disabled="enableCache && !cacheFinish" @change="switchCache"/>
+                    </el-card>
+                </el-col>
+
+                <el-col :span="6">
+                    <el-card shadow="always">
+                        <div slot="header" class="clearfix">
+                            <span class="card-title">是否已完成缓存</span>
+                            <el-button class="card-title-button" type="text" @click="clearAllCache"
+                                       v-show="enableCache && cacheFinish">清理缓存</el-button>
+                            <el-button class="card-title-button" type="text" @click="cacheAll"
+                                       v-show="enableCache && !cacheFinish">缓存所有</el-button>
+                        </div>
+                        <el-switch v-model="cacheFinish" disabled/>
+                    </el-card>
+                </el-col>
+                <el-col :span="6">
+                    <el-card shadow="always">
+                        <div slot="header" class="clearfix">
+                            <span class="card-title">已缓存文件夹</span>
+                        </div>
+                        <div class="card-content" v-text="animatedCacheDirectoryCount">
+                        </div>
+                    </el-card>
+                </el-col>
+                <el-col :span="6">
+                    <el-card shadow="always">
+                        <div slot="header" class="clearfix">
+                            <span class="card-title">已缓存文件</span>
+                        </div>
+                        <div class="card-content" v-text="animatedCacheFileCount">
+                        </div>
+                    </el-card>
+                </el-col>
+            </el-row>
+
+            <el-row>
+                <el-table
+                        height="70vh"
+                        :data="tableData.filter(data => !search || data.name.toLowerCase().includes(search.toLowerCase()))"
+                        :empty-text="emptyText"
+                        style="width: 100%;overflow-y: auto">
+                    <el-table-column
+                            prop="name"
+                            label="缓存 Key (文件夹名称)"
+                            width="550">
+                    </el-table-column>
+                    <el-table-column>
+                        <template slot="header" slot-scope="scope">
+                            <div class="table-search-input">
+                                <el-input
+                                        class="table-search-input"
+                                        v-model="search"
+                                        size="mini"
+                                        placeholder="输入关键字搜索"/>
+                            </div>
+                        </template>
+                        <template slot-scope="scope">
+                            <el-button
+                                    size="mini"
+                                    type="primary"
+                                    round
+                                    @click="refreshCache(scope.$index, scope.row)">刷新缓存</el-button>
+                        </template>
+                    </el-table-column>
+                </el-table>
+            </el-row>
+        </el-col>
     </el-row>
 </template>
 
 <script>
     import qs from 'qs';
+    import { TweenLite } from 'gsap/gsap-core'
     export default {
         name: "CacheManager",
         data() {
             return {
-                formData: {
-                    enableCache: true,
-                    cacheFinish: true,
-                    cacheCount: 0,
-                    search: ''
-                },
-                tableData: [{
-                    name:'xx'
-                }]
+                enableCache: true,
+                cacheFinish: true,
+                stopRefreshAfterLastAccess: 0,
+                refreshSeconds: 0,
+                cacheDirectoryCount: 0,
+                cacheFileCount: 0,
+                tweenedCacheDirectoryCount: 0,
+                tweenedCacheFileCount: 0,
+                search: '',
+                timer: null,
+                tableData: []
             }
         },
         methods: {
+            switchCache() {
+                if (this.enableCache) {
+                    this.openCache();
+                } else {
+                    this.closeCache();
+                }
+            },
+            openCache() {
+                this.$http.post('admin/cache/enable').then((response) => {
+                    if (response.data.code === 0) {
+                        this.$message({
+                            message: '开启成功, 正在缓存所有文件',
+                            type: 'success'
+                        });
+                    }
+                })
+            },
+            closeCache() {
+                this.$http.post('admin/cache/disable').then((response) => {
+                    if (response.data.code === 0) {
+                        this.$message({
+                            message: '关闭成功, 已清理所有缓存',
+                            type: 'success'
+                        });
+                    }
+                })
+            },
             cacheAll() {
                 this.$http.post('admin/cache/all', qs.stringify(this.form)).then(() => {
                     this.$message({
-                        message: '操作成功',
+                        message: '操作成功, 开始缓存所有文件',
                         type: 'success'
                     });
-                });            },
+                });
+            },
             clearAllCache() {
                 this.$http.post('admin/cache/clear', qs.stringify(this.form)).then(() => {
                     this.$message({
@@ -89,9 +154,10 @@
             loadConfig() {
                 this.$http.get('admin/cache/config').then((response) => {
                     let data = response.data.data;
-                    this.formData.enableCache = data.enableCache;
-                    this.formData.cacheFinish = data.cacheFinish;
-                    this.formData.cacheCount = data.cacheKeys.length;
+                    this.enableCache = data.enableCache;
+                    this.cacheFinish = data.cacheFinish;
+                    this.cacheDirectoryCount = data.cacheDirectoryCount;
+                    this.cacheFileCount = data.cacheFileCount;
 
                     let cacheKeys = data.cacheKeys;
                     cacheKeys.sort(function (a, b) {
@@ -107,6 +173,42 @@
         },
         mounted() {
             this.loadConfig();
+            if (this.timer) {
+                clearInterval(this.timer);
+            } else {
+                this.timer = setInterval(() => {
+                    this.loadConfig();
+                }, 500);
+            }
+        },
+        destroyed() {
+            clearInterval(this.timer);
+        },
+        computed: {
+            emptyText: function () {
+                if (this.enableCache === false) {
+                    return "未开启缓存";
+                }
+
+                if (this.cacheFinish === false) {
+                    return "尚未完成全部缓存";
+                }
+                return "暂无数据";
+            },
+            animatedCacheDirectoryCount: function () {
+                return this.tweenedCacheDirectoryCount.toFixed(0);
+            },
+            animatedCacheFileCount: function () {
+                return this.tweenedCacheFileCount.toFixed(0);
+            }
+        },
+        watch: {
+            cacheFileCount: function(newValue) {
+                TweenLite.to(this.$data, 0.5, { tweenedCacheFileCount: newValue });
+            },
+            cacheDirectoryCount: function(newValue) {
+                TweenLite.to(this.$data, 0.5, { tweenedCacheDirectoryCount: newValue });
+            }
         }
     }
 </script>
@@ -121,7 +223,24 @@
         margin-right: 50px;
     }
 
-    .el-button {
-        margin-right: 10px;
+    .card-title {
+        color: rgba(0,0,0,.45);
+        font-size: 14px;
+    }
+
+    .card-content {
+        color: rgba(0,0,0,.85);
+        font-size: 25px;
+        line-height: 30px;
+    }
+
+    .card-title-button {
+        float: right;
+        padding: 3px 0;
+    }
+
+    .table-search-input {
+        width: 300px;
+        float: right;
     }
 </style>
