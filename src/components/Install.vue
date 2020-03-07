6c46a47f216b81d61efbdc72025f1e10f12f6551
zhaojun1998 <root@zhaojun.im>
2020-03-07 13:45:02 +0800

    完善初始化页面表单及校验

--- src/components/Install.vue
+++ src/components/Install.vue
@@ -1,6 +1,6 @@
 <template>
     <el-row :gutter="20">
-        <el-col :span="8" :offset="8">
+        <el-col :span="10" :offset="7">
             <el-card class="box-card" align-center shadow="always">
                 <el-form ref="form" :rules="rules" :model="form" label-width="auto" :status-icon="true" v-loading="loading" element-loading-text="保存并初始化中.">
                     <el-form-item label="站点名称" prop="siteName">
@@ -28,13 +28,47 @@
                     <el-form-item
                             v-for="(item) in storageStrategyForm"
                             :label="item.title"
-                            :key="item.title">
+                            :key="item.title"
+                            :prop="'storageStrategyForm' + item.key">
 
                         <el-select v-model="form.storageStrategyConfig.endPoint"
                                    v-if="item.key === 'endPoint' && region.hasOwnProperty(form.storageStrategy)">
                             <el-option v-for="endPoint in region[form.storageStrategy]" :label="endPoint.name" :value="endPoint.val" :key="endPoint.name"/>
                         </el-select>
-                        <el-input v-else v-model.trim="form.storageStrategyConfig[item.key]"/>
+
+                        <div v-else-if="item.key === 'pathStyle'">
+                            <el-select v-model="form.storageStrategyConfig.pathStyle" style="width: 50%">
+                                <el-option label="bucket-virtual-hosting" value="bucket-virtual-hosting"></el-option>
+                                <el-option label="path-style" value="path-style"></el-option>
+                            </el-select>
+                            <el-link class="zfile-word-aux" target="_blank" icon="el-icon-document"
+                                     href="https://docs.aws.amazon.com/zh_cn/AmazonS3/latest/dev/VirtualHosting.html#path-style-access">查看 S3 API 说明文档</el-link>
+                        </div>
+
+                        <div v-else-if="item.key === 'isPrivate'">
+                            <el-switch v-model="form.storageStrategyConfig.isPrivate" />
+                            <span class="zfile-word-aux">私有空间会生成带签名的下载链接</span>
+                        </div>
+
+                        <el-input placeholder="" @input="change($event)" v-else v-model="form.storageStrategyConfig[item.key]"/>
+
+                        <div v-if="item.key === 'base-path'">
+                            <span class="zfile-word-aux" style="margin-left: 0">基路径表示从哪个路径开始文件, 不填写表示从根开始</span>
+                        </div>
+
+                        <div v-if="item.key === 'domain' && form.storageStrategy === 'ftp'">
+                            <span class="zfile-word-aux" style="margin-left: 0">此域名表示 http 访问域名</span>
+                        </div>
+                    </el-form-item>
+
+                    <el-form-item v-if="form.storageStrategy === 'onedrive'">
+                        <el-link target="_blank" icon="el-icon-edit"
+                                 href="https://login.microsoftonline.com/common/oauth2/v2.0/authorize?client_id=09939809-c617-43c8-a220-a93c1513c5d4&response_type=code&redirect_uri=https://zfile.jun6.net/onedrive/callback&scope=offline_access%20User.Read%20Files.ReadWrite.All">前往获取授权</el-link>
+                    </el-form-item>
+
+                    <el-form-item v-if="form.storageStrategy === 'onedrive-china'">
+                        <el-link target="_blank" icon="el-icon-edit"
+                                 href="https://login.chinacloudapi.cn/common/oauth2/v2.0/authorize?client_id=4a72d927-1907-488d-9eb2-1b465c53c1c5&response_type=code&redirect_uri=https://zfile.jun6.net/onedrive/china-callback&scope=offline_access%20User.Read%20Files.ReadWrite.All">前往获取授权</el-link>
                     </el-form-item>
 
                     <el-form-item>
@@ -64,8 +98,8 @@
                     storageStrategyConfig: {
                         endPoint: '',
                     },
-                    loading: true
                 },
+                loading: false,
                 storageStrategyForm: [],
                 supportStrategy: [],
                 region: region,
@@ -82,6 +116,25 @@
                     domain: [
                         {required: true, type: 'url', message: '请输入正确的域名, 需以 http:// 或 https:// 开头', trigger: 'change'},
                     ],
+                    storageStrategyFormdomain: [
+                        {
+                            required: this.storageStrategy !== 'ftp',
+                            type: 'url',
+                            message: '请输入正确的域名, 需以 http:// 或 https:// 开头',
+                            trigger: 'change'
+                        }
+                    ],
+                    storageStrategyFormusername: [{required: this.storageStrategy === 'upyun', message: '操作员名称不能为空'}],
+                    storageStrategyFormpassword: [{required: this.storageStrategy === 'upyun', message: '操作员密码不能为空'}],
+                    storageStrategyFormendPoint: [{required: true, message: '区域不能为空'}],
+                    storageStrategyFormaccessKey: [{required: true, message: 'AccessKey 不能为空'}],
+                    storageStrategyFormsecretKey: [{required: true, message: 'SecretKey 不能为空'}],
+                    'storageStrategyFormbucket-name': [{required: true, message: '此项不能为空'}],
+                    storageStrategyFormhost: [{required: true, message: "域名或 IP 不能为空"}],
+                    storageStrategyFormport: [{required: true, message: "端口不能为空"}],
+                    storageStrategyFormaccessToken: [{required: true, message: "访问令牌不能为空"}],
+                    storageStrategyFormrefreshToken: [{required: true, message: "刷新令牌不能为空"}],
+                    storageStrategyFormsecretId: [{required: true, message: "SecretId 不能为空"}]
                 }
             };
         },
@@ -94,10 +147,13 @@
             }
         },
         methods: {
+            change() {
+                this.$forceUpdate();
+            },
             submitForm(formName) {
-                this.loading = true;
                 this.$refs[formName].validate((valid) => {
                     if (valid) {
+                        this.loading = true;
                         let that = this;
                         this.$http.post('install', qs.stringify(this.form)).then((response) => {
                             this.loading = false;
@@ -112,6 +168,7 @@
                             });
                         })
                     } else {
+                        this.loading = false;
                         return false;
                     }
                 });
@@ -137,12 +194,18 @@
     .box-card {
         padding-top: 30px;
         padding-right: 30px;
-        margin: 15vh auto;
-        height: 65vh;
+        margin: 10vh auto;
+        height: 75vh;
         overflow-y: auto;
     }
 
     .el-select {
         width: 100%;
+    }
+
+
+    .zfile-word-aux {
+        margin-left: 20px;
+        color: #aaaaaa;
     }
 </style>
