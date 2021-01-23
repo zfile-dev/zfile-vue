61e69a35e75f2b79e0f5d959b393ce8035f7bb68
zhaojun1998 <root@zhaojun.im>
2021-01-23 14:48:24 +0800

    :sparkles: 文件夹密码修改为输入时不可见

--- src/components/List.vue
+++ src/components/List.vue
@@ -368,6 +368,7 @@
                 this.$prompt('请输入密码', '提示', {
                     confirmButtonText: '确定',
                     cancelButtonText: '取消',
+                    inputType: 'password',
                     inputValidator(val) {
                         return !!val
                     },
