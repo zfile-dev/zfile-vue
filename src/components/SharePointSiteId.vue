bd0bc148a9e0e32158e512aaf18383f182e2c27b
zhaojun1998 <root@zhaojun.im>
2021-02-01 23:07:05 +0800

    :fire: 移除无用代码

--- src/components/SharePointSiteId.vue
+++ src/components/SharePointSiteId.vue
@@ -37,15 +37,6 @@
                 </el-form-item>
             </el-form>
         </el-col>
-
-        <el-input placeholder="请输入内容" v-model="input3" class="input-with-select">
-            <el-select v-model="select" slot="prepend" placeholder="请选择">
-                <el-option label="餐厅名" value="1"></el-option>
-                <el-option label="订单号" value="2"></el-option>
-                <el-option label="用户电话" value="3"></el-option>
-            </el-select>
-            <el-button slot="append" icon="el-icon-search"></el-button>
-        </el-input>
     </el-row>
 </template>
 
@@ -54,10 +45,6 @@ export default {
     name: "SharePointSiteId",
     data() {
         return {
-            input1: '',
-            input2: '',
-            input3: '',
-            select: '',
             form: {
                 type: 'Standard',
                 accessToken: '',
