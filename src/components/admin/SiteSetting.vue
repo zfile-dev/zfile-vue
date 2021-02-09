9decaab5b089f6e8d3c5edfbe965d836970e4fd0
zhaojun1998 <root@zhaojun.im>
2021-02-09 11:15:50 +0800

    :lipstick: 管理员站点维护首页增加动画效果

--- src/components/admin/SiteSetting.vue
+++ src/components/admin/SiteSetting.vue
@@ -4,15 +4,16 @@
                  v-loading="loading"
                  element-loading-text="保存中..."
                  :rules="rules" ref="form" :model="form" label-width="auto" :status-icon="true">
-            <el-form-item label="站点名称" prop="siteName">
+            <el-form-item label="站点名称" prop="siteName" class="box animate__animated animate__fadeInUp">
                 <el-input v-model="form.siteName"/>
             </el-form-item>
 
-            <el-form-item label="站点域名" prop="domain">
+            <el-form-item label="站点域名" prop="domain" class="box animate__animated animate__fadeInUp">
                 <el-input v-model="form.domain"/>
+                <div class="zfile-word-aux zfile-margin-left-unset">此地址用于生成直链及本次存储下载使用，请务必保持和服务端地址一样。</div>
             </el-form-item>
 
-            <el-form-item>
+            <el-form-item class="box animate__animated animate__fadeInUp">
                 <el-button type="primary" size="small" @click="submitForm('form')">保存设置</el-button>
             </el-form-item>
         </el-form>
@@ -69,6 +70,37 @@
         mounted() {
             this.$http.get('admin/config').then((response) => {
                 this.form = response.data.data;
+
+                console.log(this.$http.defaults.baseURL);
+                let serverDomain = this.$http.defaults.baseURL === "" ? window.location.origin : this.$http.defaults.baseURL;
+                let siteDomain = this.form.domain;
+
+                if (serverDomain !== siteDomain) {
+                    this.$confirm(`检测到服务端地址为 ${serverDomain}，当前配置站点域名为 ${siteDomain}，是否自动进行修正？`, '提示', {
+                        confirmButtonText: '确定',
+                        cancelButtonText: '取消',
+                        type: 'warning',
+                        callback: action => {
+                            if (action === 'confirm') {
+                                this.form.domain = serverDomain;
+                                this.$http.post('admin/config', qs.stringify(this.form)).then((response) => {
+                                    this.loading = false;
+                                    if (response.data.code === this.common.responseCode.SUCCESS) {
+                                        this.$message({
+                                            message: '修正成功',
+                                            type: 'success'
+                                        });
+                                    } else {
+                                        this.$message({
+                                            message: response.data.msg,
+                                            type: 'error'
+                                        });
+                                    }
+                                })
+                            }
+                        }
+                    });
+                }
             });
         }
     }
