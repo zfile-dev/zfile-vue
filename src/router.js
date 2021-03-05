c42ed506e8e6f1971f919f5de12a75936727d0a7
zhaojun1998 <root@zhaojun.im>
2021-03-05 21:49:31 +0800

    :zap: 优化打包逻辑，不同模块打包成不同文件，优化页面加载速度

--- src/router.js
+++ src/router.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import Router from "vue-router";
 Vue.use(Router);
 
-const Install = () => import(/* webpackChunkName: "front" */'./components/Install');
+const Install = () => import(/* webpackChunkName: "install" */'./components/Install');
 const Main = () => import(/* webpackChunkName: "front" */'./components/Main');
 
 const Admin = () => import(/* webpackChunkName: "admin" */'./components/admin/Index');
@@ -19,6 +19,10 @@ export default new Router({
     mode: 'hash', // 路由模式:默认为 hash,如果改为 history,则需要后端进行配合
     base: '/', // 基路径:默认值为'/'.如果整个单页应用在/app/下,base 就应该设为'/app/'.一般可以写成__dirname,在 webpack 中配置.
     routes: [
+        {
+            path: '/',
+            redirect: 'main',
+        },
         {
             path: '/install',
             component: Install
