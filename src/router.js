d89d1e15f87e0e540e193a02683b3ff698b9ddce
zhaojun1998 <root@zhaojun.im>
2021-03-10 21:38:28 +0800

    :sparkles: 去除重复路由提示

--- src/router.js
+++ src/router.js
@@ -2,6 +2,11 @@ import Vue from 'vue';
 import Router from "vue-router";
 Vue.use(Router);
 
+const originalPush = Router.prototype.push
+Router.prototype.push = function push(location) {
+    return originalPush.call(this, location).catch(err => err)
+}
+
 const Install = () => import(/* webpackChunkName: "install" */'./components/Install');
 const Main = () => import(/* webpackChunkName: "front" */'./components/Main');
 
