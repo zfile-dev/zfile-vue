0331365d2e1a82865f808d30203bcbd82e22ca04
zhaojun <873019219@qq.com>
2022-08-05 15:12:51 +0800

    :hammer: 重构代码，抽取公共 router

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,11 @@
 <template>
 	<router-view />
 </template>
+
+<script setup>
+import useRouterData from "~/composables/useRouterData";
+
+let router = useRouter();
+let route = useRoute();
+useRouterData(router, route);
+</script>
