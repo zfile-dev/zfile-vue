f201c92a538d7ba00d4d6b7c574653a0e7add4c8
zhaojun <873019219@qq.com>
2022-08-11 20:58:54 +0800

    :sparkles: 增加浏览器信息输出, 便于问题发现.

--- src/App.vue
+++ src/App.vue
@@ -8,4 +8,7 @@ import useRouterData from "~/composables/useRouterData";
 let router = useRouter();
 let route = useRoute();
 useRouterData(router, route);
+
+import uaBrowser from 'ua-browser'
+console.log('当前浏览器信息:', uaBrowser());
 </script>
