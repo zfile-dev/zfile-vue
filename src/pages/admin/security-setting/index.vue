1c42af41f78b72e58a16140c3fe2253f56463d0b
zhaojun <873019219@qq.com>
2025-06-10 20:03:04 +0800

    系统设置页面，双因素认证配置优化，页面新增保存按钮，而不是之前的切换保存。

--- src/pages/admin/security-setting/index.vue
+++ src/pages/admin/security-setting/index.vue
@@ -12,22 +12,24 @@
     <admin-form-header title="安全设置" sub-title="此处设置可保护您的站点安全" />
 
     <el-form-item label="是否显示登录入口">
-      <el-switch v-model="siteSetting.showLogin" @change="saveData"></el-switch>
+      <el-switch v-model="siteSetting.showLogin"></el-switch>
       <div class="el-form-item-tips">
         启用后，会在首页上方工具栏显示后台登录入口，请根据自身情况选择是否启用。(这里只是控制是否显示，直接访问登录页 URL: <span class="text-blue-400">{{currentLocale}}/login</span> 也能访问)
       </div>
     </el-form-item>
 
     <el-form-item label="匿名用户首页显示">
-        <el-input type="textarea" :rows="8" v-model="siteSetting.guestIndexHtml" @change="saveData"></el-input>
+        <el-input type="textarea" :rows="8" v-model="siteSetting.guestIndexHtml"></el-input>
         <div class="el-form-item-tips">
           可填写 html 代码，用于强制匿名用户访问首页时显示的内容，留空则显示默认首页内容。<br>
             如强制跳转到登录页，可填写 <code class="code select-all">&lt;script&gt;location.href = '/login';&lt;/script&gt;</code>
+	        <br>
+	        <span class="text-red-400">注意：如果你网站配置了防火墙，可能会被当成恶意请求拦截而保存失败。</span>
         </div>
     </el-form-item>
 
     <el-form-item label="登录日志模式">
-      <el-radio-group v-model="siteSetting.loginLogMode" @change="saveData">
+      <el-radio-group v-model="siteSetting.loginLogMode">
         <el-radio value="off">关闭</el-radio>
         <el-radio value="all">全部记录</el-radio>
         <el-radio value="ignoreSuccessPwd">不记录成功密码</el-radio>
@@ -39,7 +41,7 @@
     </el-form-item>
 
     <el-form-item label="是否启用图片验证码">
-      <el-switch v-model="siteSetting.loginImgVerify" @change="saveData"></el-switch>
+      <el-switch v-model="siteSetting.loginImgVerify"></el-switch>
       <div class="el-form-item-tips">
         启用图片验证码可以防止批量恶意登录，但将密码设置为高强度才是最有效的防止恶意登录的方法。
       </div>
@@ -50,68 +52,33 @@
       <div class="el-form-item-tips">
         双因素认证可完美保护登录认证，可使用任意支持 2FA 认证的软件，如 <b>Google 身份验证器</b>、<b>Microsoft Authenticator</b> 等软件.（仅适用于管理员登录，不影响其他用户登录。）
       </div>
-		</el-form-item>
-
-		<el-form-item label="双因素认证密钥" v-if="siteSetting.adminTwoFactorVerify">
-			<div class="w-full" v-if="siteSetting.loginVerifySecret">
+			<div class="w-full" v-if="siteSetting.adminTwoFactorVerify && siteSetting.loginVerifySecret">
 				<el-button type="primary" @click="rebind">重新绑定</el-button>
 				<el-button type="primary" @click="cancelBind">取消绑定</el-button>
 				<el-button type="primary" @click="validBind">验证绑定</el-button>
 			</div>
-			<div v-else>
-				<el-image @click="load2FAInfo" :src="twoFAData?.qrcode"></el-image>
-				<el-input readonly v-model="twoFAData.secret">
-					<template #append>
-						<el-tooltip class="item" effect="dark" content="复制" placement="bottom">
-							<el-button @click="copyTwoFASecret" :icon="ClipboardDocumentIcon"></el-button>
-						</el-tooltip>
-					</template>
-				</el-input>
-			</div>
-			<div class="el-form-item-tips" v-if="!siteSetting.loginVerifySecret">
-				可扫描上方二维码或复制下方密钥到支持双因素认证的软件中添加。
-			</div>
 		</el-form-item>
 
-		<el-form-item label="双因素认证验证码" v-if="siteSetting.adminTwoFactorVerify && !siteSetting.loginVerifySecret">
-			<el-input v-model="twoFAData.code">
-			</el-input>
-			<el-button class="mt-2" @click="verifyCodeAndBind" type="primary">验证并绑定</el-button>
-			<div class="el-form-item-tips">
-				请绑定双因素认证软件后，输入显示的验证码进行绑定
-			</div>
-		</el-form-item>
+	  <el-form-item class="admin-from-footer">
+		  <el-button class="ml-auto" type="primary" :icon="CheckBadgeIcon" @click="beforeSave">保存设置</el-button>
+	  </el-form-item>
 	</el-form>
+
+	<two-fa-dialog :visible="twoFaDialogShow" @close="onTwoFaDialogClose" />
 </template>
 
 <script setup>
-import { ClipboardDocumentIcon } from '@heroicons/vue/24/outline'
-import { toClipboard } from "@soerenmartius/vue3-clipboard";
+import { CheckBadgeIcon } from '@heroicons/vue/24/outline'
 
 import { updateSecuritySettingReq } from "~/api/admin/admin-setting";
-import { generator2FAInfoReq, verify2FAInfoReq } from "~/api/admin/admin-2fa";
+import { verify2FAInfoReq } from "~/api/admin/admin-2fa";
 
 import useGlobalConfigStore from "~/stores/global-config";
 const globalConfigStore = useGlobalConfigStore();
 
 import useAdminSetting from "~/composables/admin/useAdminSetting";
-const { siteSetting, siteSettingReload, saveData, saveLoading } = useAdminSetting(updateSecuritySettingReq);
-
-const twoFAData = ref({});
-// 加载 2fa 认证信息
-const load2FAInfo = () => {
-  generator2FAInfoReq().then((res) => {
-    twoFAData.value = res.data;
-  })
-}
-
-// 验证并绑定双因素认证
-const verifyCodeAndBind = () => {
-  verify2FAInfoReq(twoFAData.value).then((res) => {
-    ElMessage.success('绑定成功');
-    siteSettingReload();
-  })
-}
+import TwoFaDialog from "~/pages/admin/security-setting/two-fa-dialog.vue";
+const { siteSetting, saveData, saveLoading } = useAdminSetting(updateSecuritySettingReq);
 
 // 绑定成功后，手动验证双因素认证码.
 const validBind = () => {
@@ -133,46 +100,46 @@ const validBind = () => {
   });
 }
 
-// 复制 2fa 密钥
-const copyTwoFASecret = () => {
-  toClipboard(twoFAData.value.secret).then(() => {
-    ElMessage.success('复制成功');
-  })
-}
-
 // 重新绑定
 const rebind = () => {
   siteSetting.value.loginVerifySecret = '';
-  load2FAInfo();
+	twoFaDialogShow.value = true;
 }
 
 // 取消绑定
 const cancelBind = () => {
-  ElMessageBox.confirm('是否确认取消绑定，确认后将关闭双因素认证。', '提示', {
-    confirmButtonText: '确定',
-    cancelButtonText: '取消',
-    type: 'success',
-    callback: action => {
-      if (action === 'confirm') {
-        siteSetting.value.loginVerifySecret = '';
-        siteSetting.value.adminTwoFactorVerify = false;
-      }
-    }
-  });
+	siteSetting.value.loginVerifySecret = '';
+	siteSetting.value.adminTwoFactorVerify = false;
 }
 
 const adminTwoFactorVerifyOnChange = () => {
   if (siteSetting.value.adminTwoFactorVerify === false) {
     siteSetting.value.loginVerifySecret = '';
-    saveData();
   }
 
   if (siteSetting.value.adminTwoFactorVerify && !siteSetting.value.loginVerifySecret) {
-    load2FAInfo();
+	  twoFaDialogShow.value = true;
   }
 }
 
+const beforeSave = () => {
+	if (siteSetting.value.adminTwoFactorVerify === false) {
+		siteSetting.value.loginVerifySecret = '';
+	}
+
+	saveData();
+};
+
 const currentLocale = ref(window.location.origin);
+const twoFaDialogShow = ref(false);
+const onTwoFaDialogClose = (secret) => {
+	if (secret) {
+		siteSetting.value.loginVerifySecret = secret;
+	} else {
+		siteSetting.value.adminTwoFactorVerify = false;
+	}
+	twoFaDialogShow.value = false;
+};
 </script>
 
 <style lang="scss" scoped>
---
+++ src/pages/admin/security-setting/two-fa-dialog.vue
@@ -0,0 +1,80 @@
+<template>
+	<el-dialog append-to-body
+	           v-model="isShow" :destroy-on-close="true"
+	           @close="closeDialog"
+	           title="绑定双因素认证"
+	           draggable
+	           top="5vh"
+	           width="30%">
+		<div class="text-center">
+			<div>可使用双因素认证软件扫描下方二维码或复制下方密钥到软件中添加。</div>
+			<el-image class="block" @click="load2FAInfo" :src="twoFAData?.qrcode" />
+			<div @click="copyTwoFASecret" class="text-gray-300 mb-4">{{ twoFAData.secret }}</div>
+			<div class="flex justify-between space-x-2">
+				<el-input v-model="twoFAData.code" placeholder="请绑定双因素认证软件后，输入显示的验证码。"/>
+				<el-button :loading="saveLoading" type="primary" @click="verifyCodeAndBind">验证并绑定</el-button>
+			</div>
+		</div>
+	</el-dialog>
+</template>
+
+<script setup>
+import { generator2FAInfoReq, verify2FAInfoReq } from "~/api/admin/admin-2fa";
+import { toClipboard } from "@soerenmartius/vue3-clipboard";
+
+const emit = defineEmits(['close']);
+const props = defineProps({
+	visible: {
+		type: Boolean,
+		required: true
+	}
+});
+
+const isShow = ref(props.visible);
+const isSuccess = ref(false);
+const saveLoading = ref(false);
+
+watch(() => props.visible, (newVal) => {
+	isShow.value = newVal;
+	if (newVal) {
+		isSuccess.value = false;
+		load2FAInfo();
+	}
+});
+
+const closeDialog = () => {
+	if (!isSuccess.value) {
+		emit('close', '');
+	}
+}
+
+const twoFAData = ref({});
+const load2FAInfo = () => {
+	generator2FAInfoReq().then((res) => {
+		twoFAData.value = res.data;
+	})
+}
+
+// 验证并绑定双因素认证
+const verifyCodeAndBind = () => {
+	saveLoading.value = true
+	verify2FAInfoReq(twoFAData.value).then(() => {
+		ElMessage.success('绑定成功');
+		isSuccess.value = true;
+		emit('close', twoFAData.value.secret);
+	}).finally(() => {
+		saveLoading.value = false;
+	});
+}
+
+// 复制 2fa 密钥
+const copyTwoFASecret = () => {
+	toClipboard(twoFAData.value.secret).then(() => {
+		ElMessage.success('复制成功');
+	})
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
