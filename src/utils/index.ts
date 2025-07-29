e065da928034eff5b5f3b5c15f3efe9e3e42714f
zhaojun <873019219@qq.com>
2025-07-29 20:52:37 +0800

    去除无用依赖

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,7 +2,6 @@ export * from "./models/base";
 export * from "./models/file";
 export * from "./models/path";
 export * from "./models/qrcode";
-export * from "./models/rule";
 export * from "./models/time";
 export * from "./models/util";
 export * from "./models/window";
